3f849b63a03b93b469ac5f7b6f55c97f3ad4e04d
adonayeshetayehu <115351354+adonayeshetayehu@users.noreply.github.com>
2026-02-23 17:22:26 +0100

    Expand grid search for all models and update training pipelines

--- src/train_model.py
+++ src/train_model.py
@@ -11,26 +11,43 @@ def train_model(preprocessor, X_train, y_train, model_name="RandomForest"):
     if model_name == "RandomForest":
         classifier = RandomForestClassifier(random_state=42)
         param_grid = {
-            'classifier__n_estimators':[50,100],
+            'classifier__n_estimators':[50,100,200],
             'classifier__max_depth':[None,10,20],
-            'classifier__min_samples_split':[2,5]
+            'classifier__min_samples_split':[2,5,10]
         }
     elif model_name == "LogisticRegression":
         classifier = LogisticRegression(random_state=42)
         param_grid = {
-            'classifier__solver':['liblinear'],
-            'classifier__penalty':['l1','l2'],
+            'classifier__solver':['liblinear', 'saga'],
+            'classifier__penalty':['l1','l2', 'elasticnet'],
             'classifier__class_weight':[None,'balanced']
         }
     elif model_name == "SVC":
         classifier = SVC(random_state=42)
-        param_grid = {'classifier__C':[0.1,1,10]}
+        param_grid = {
+            'classifier__C':[0.1,1,10, 50],
+            'classifier__kernel':['linear','rbf', 'poly'],
+            'classifier__gamma':['scale','auto'],
+        }
     elif model_name == "XGBoost":
         classifier = XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42)
-        param_grid = {'classifier__n_estimators':[50,100]}
+        param_grid = {
+            'classifier__n_estimators':[50,100,200],
+            'classifier__max_depth':[3,5,7],
+            'classifier__learning_rate':[0.01,0.05],
+            'classifier__subsample':[0.6,0.8],
+            'classifier__colsample_bytree':[0.6,0.8]
+        }
     elif model_name == "LightGBM":
         classifier = LGBMClassifier(random_state=42)
-        param_grid = {'classifier__n_estimators':[50,100]}
+        param_grid = {
+            'classifier__n_estimators':[50,100,200],
+            'classifier__max_depth':[3,5,7],
+            'classifier__learning_rate':[0.01,0.05,0.1],
+            'classifier__num_leaves':[15,31,63],
+            'classifier__subsample':[0.6,0.8],
+            'classifier__colsample_bytree':[0.6,0.8]
+        }
     else:
         raise ValueError(f"Unknown model_name {model_name}")
 
